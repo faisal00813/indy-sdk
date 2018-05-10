78425bdb690c10ab16add1c1bd7447e0c83a8b04
Mohammad Abdul Sami <faisal00813@gmail.com>
2018-05-10 19:49:14 +0530

    Fixed downloading of deps in android_build.sh

--- libindy/android_build.sh
+++ libindy/android_build.sh
@@ -26,24 +26,24 @@ LIBZMQ=libzmq_4.2.2
 OPENSSL=openssl_1.1.0c
 LIBZ=libz_1.2.11
 
-#
-###cleanup
-# rm -rf ${ANDROID_PREBUILT_BINARIES}
-#
-##Download prebuilt deps
-#mkdir ${ANDROID_PREBUILT_BINARIES}
-#pushd ${ANDROID_PREBUILT_BINARIES}
-#curl -L -o $LIBSODIUM.zip https://repo.sovrin.org/test/sdk-prebuilt-deps/android/deps/armv7/$LIBSODIUM.zip
-#curl -L -o $LIBZMQ.zip https://repo.sovrin.org/test/sdk-prebuilt-deps/android/deps/armv7/$LIBZMQ.zip
-#curl -L -o $OPENSSL.zip https://repo.sovrin.org/test/sdk-prebuilt-deps/android/deps/armv7/$OPENSSL.zip
-#curl -L -o $LIBZ.zip https://repo.sovrin.org/test/sdk-prebuilt-deps/android/deps/armv7/$LIBZ.zip
-#
-## #extract deps
-#unzip -qq $LIBSODIUM.zip
-#unzip -qq $LIBZMQ.zip
-#unzip -qq $OPENSSL.zip
-#unzip -qq $LIBZ.zip
-#popd
+
+##cleanup
+ rm -rf ${ANDROID_PREBUILT_BINARIES}
+
+#Download prebuilt deps
+mkdir ${ANDROID_PREBUILT_BINARIES}
+pushd ${ANDROID_PREBUILT_BINARIES}
+curl -L -o $LIBSODIUM.zip https://repo.sovrin.org/test/sdk-prebuilt-deps/android/deps/armv7/$LIBSODIUM.zip
+curl -L -o $LIBZMQ.zip https://repo.sovrin.org/test/sdk-prebuilt-deps/android/deps/armv7/$LIBZMQ.zip
+curl -L -o $OPENSSL.zip https://repo.sovrin.org/test/sdk-prebuilt-deps/android/deps/armv7/$OPENSSL.zip
+curl -L -o $LIBZ.zip https://repo.sovrin.org/test/sdk-prebuilt-deps/android/deps/armv7/$LIBZ.zip
+
+# #extract deps
+unzip -qq $LIBSODIUM.zip
+unzip -qq $LIBZMQ.zip
+unzip -qq $OPENSSL.zip
+unzip -qq $LIBZ.zip
+popd
 
 #setup paths for deps
 export SODIUM_LIB_DIR=${ANDROID_PREBUILT_BINARIES}/$LIBSODIUM/lib
